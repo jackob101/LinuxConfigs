6b292a2ebf9517398b11096a0d5ccd5b9988fd1d
Jakub Pietrzyk <pietrzyk.jakub001@gmail.com>
2024-07-24 21:16:38 +0200

    fix: somehow swww lags fixed itself?

--- hypr/.config/hypr/scripts/random_wallpaper.sh
+++ hypr/.config/hypr/scripts/random_wallpaper.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-SWWW_TRANSITION_STEP=60
-SWWW_TRANSITION_FPS=144
-
 path_to_wallpaper=$(find "$WALLHEAVEN_SYNC_STORAGE_PATH/walls/" -maxdepth 1 -type f | grep -v -E "index.json" | shuf -n 1)
-swww img --transition-type wipe --transition-angle 0 "$path_to_wallpaper"
+swww img --transition-type outer --transition-step 180 --transition-duration 2 --transition-fps 144 "$path_to_wallpaper"
