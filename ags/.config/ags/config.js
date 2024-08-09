f36b15847e9468f70856ea1f5be58ec0bf19fd2b
Jackob Pietrzyk <pietrzyk.jakub001@gmail.com>
2024-08-09 22:48:48 +0200

    feat: add shamelessly stolen notificatin popups

--- ags/.config/ags/config.js
+++ ags/.config/ags/config.js
@@ -1,6 +1,7 @@
 import Gtk from "gi://Gtk?version=3.0"
 import { Bar } from "./widgets/bar/init.js"
 import { ExitScreen } from "./widgets/exit_screen/init.js"
+import { NotificationPopups } from "./widgets/notification_poup/init.js"
 
 const hyprland = await Service.import("hyprland")
 
@@ -19,6 +20,7 @@ Utils.exec(`sassc ${scss} ${css}`)
 */
 let windows = hyprland.monitors.map(e => Bar(e.name, e.id));
 windows.push(ExitScreen())
+windows.push(NotificationPopups())
 
 App.config({
     style: "./style.css",
