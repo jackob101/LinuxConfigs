9a548dbfe30bf11633ac99962df940a406e29d2c
jackob101 <pietrzyk.jakub001@gmail.com>
2025-01-26 21:31:53 +0100

    feat: add cargo bin location to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -19,6 +19,7 @@ if status is-interactive
     fish_add_path $HOME/go/bin/
     fish_add_path $HOME/jetbrains/intellij/bin/
     fish_add_path $HOME/.local/share/coursier/bin/
+    fish_add_path $HOME/.cargo/bin/
     set GOBIN $HOME/go/bin
 
     alias books='set book (ls $HOME/Documents/books | fzf); zathura $HOME/Documents/books/$book & disown && exit'
