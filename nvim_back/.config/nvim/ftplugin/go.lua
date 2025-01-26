6d200fc7a08e6ce6cdbb6d28018ebfcb20ae8add
jackob101 <pietrzyk.jakub001@gmail.com>
2025-01-26 21:34:12 +0100

    cleanup: remove old nvim config

--- nvim_back/.config/nvim/ftplugin/go.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.shiftwidth = 8
-vim.opt.tabstop = 8
-vim.opt.expandtab = true
--- nvim_back/.config/nvim/lua/filetypes.lua
+++
@@ -1,6 +0,0 @@
-vim.filetype.add({
-	extension = {
-		nix = "nix",
-	},
-	pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
-})
