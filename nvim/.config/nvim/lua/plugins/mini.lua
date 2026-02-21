e42ab17510a3b28308b79fcc2dbaa00a9b092334
jackob101 <pietrzyk.jakub001@gmail.com>
2026-02-21 22:46:43 +0100

    remove telescope and use mini.pick

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -19,6 +19,38 @@ return {
 			-- - sr)'  - [S]urround [R]eplace [)] [']
 			require("mini.surround").setup()
 
+			local win_config = function(percent_width, percent_height)
+				return function()
+					local height = math.floor(0.618 * vim.o.lines)
+					local width = math.floor(0.618 * vim.o.columns)
+					return {
+						anchor = "NW",
+						height = height,
+						width = width,
+						row = math.floor(percent_height * (vim.o.lines - height)),
+						col = math.floor(percent_width * (vim.o.columns - width)),
+					}
+				end
+			end
+
+			require("mini.pick").setup({
+
+				window = { config = win_config(0.5, 0.5) },
+			})
+			require("mini.extra").setup()
+
+			vim.keymap.set("n", "<leader>pk", MiniExtra.pickers.keymaps, { desc = "Keymaps" })
+			vim.keymap.set("n", "<leader>pf", MiniPick.builtin.files, { desc = "Files on root" })
+			vim.keymap.set("n", "<leader>pr", MiniPick.builtin.resume, { desc = "Resume previous pick" })
+			vim.keymap.set("n", "<leader><leader>", MiniPick.builtin.buffers, { desc = "Open buffers" })
+			vim.keymap.set("n", "<leader>pg", function()
+				MiniPick.builtin.grep({ pattern = "" })
+			end, { desc = "Grep on root dir" })
+			vim.keymap.set("n", "<leader>p/", function()
+				MiniPick.builtin.grep({ pattern = "", globs = { vim.fn.expand("%") } })
+			end, { desc = "Current buffer grep" })
+			vim.keymap.set("n", "<leader>pd", MiniExtra.pickers.diagnostic, { desc = "Diagnostics" })
+
 			-- Simple and easy statusline.
 			--  You could remove this setup call if you don't like it,
 			--  and try some other statusline plugin
